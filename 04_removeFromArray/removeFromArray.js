3d193239df250d169bfe16d9f9ed8ad6cf2e6817
Naoto <naotojacob@gmail.com>
2023-06-26 13:30:49 -0600

    Finish exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    for (let arg of args) {
+        for (let i = 0; i < array.length; i++) {
+            if (arg === array[i]) {
+                array.splice(i, 1)
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
