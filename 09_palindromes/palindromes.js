690f0d1c0c7e1e8c1447c2a357f0f8cfceaf78ea
Naoto <naotojacob@gmail.com>
2023-06-27 18:36:25 -0600

    Finish exercises 9 and 10.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuation = /[\.,?!\s]/g;
+    let reversedString = string.split("").reverse().join();   
+    return string.toLowerCase().replace(punctuation, "") === reversedString.toLowerCase().replace(punctuation,"");
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS"
+    }
+    if (index == 1 || index == 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(index - 1) + fibonacci(index - 2);
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
