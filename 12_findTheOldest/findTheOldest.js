698b68ffe4af5157ad588ff1a0bf270a6cb5d1e5
Naoto <naotojacob@gmail.com>
2023-06-28 15:02:57 -0600

    Finish exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    const currDate = new Date();
+    let maxAge = 0;
+    let oldestPersonIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].yearOfDeath) {
+            let currAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            if (currAge > maxAge) {
+                maxAge = currAge;
+                oldestPersonIndex = i;
+            }
+        }
+        else {
+            let currAge = currDate.getFullYear() - arr[i].yearOfBirth;
+            if (currAge > maxAge) {
+                maxAge = currAge;
+                oldestPersonIndex = i;
+            }
+        }
+    }
+    return arr[oldestPersonIndex]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
